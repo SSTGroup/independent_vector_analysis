dc116e41d3708088041696e06e91ee281b6a5539
Isabell Lehmann <isabell.lehmann@sst.upb.de>
2021-10-20 11:03:01 +0200

    add SST header in each file

--- independent_vector_analysis/initializations.py
+++ independent_vector_analysis/initializations.py
@@ -1,3 +1,43 @@
+#     Copyright (c) <2021> <University of Paderborn>
+#     Signal and System Theory Group, Univ. of Paderborn, https://sst-group.org/
+#     https://github.com/SSTGroup/independent_vector_analysis
+#
+#     Permission is hereby granted, free of charge, to any person
+#     obtaining a copy of this software and associated documentation
+#     files (the "Software"), to deal in the Software without restriction,
+#     including without limitation the rights to use, copy, modify and
+#     merge the Software, subject to the following conditions:
+#
+#     1.) The Software is used for non-commercial research and
+#        education purposes.
+#
+#     2.) The above copyright notice and this permission notice shall be
+#        included in all copies or substantial portions of the Software.
+#
+#     3.) Publication, Distribution, Sublicensing, and/or Selling of
+#        copies or parts of the Software requires special agreements
+#        with the University of Paderborn and is in general not permitted.
+#
+#     4.) Modifications or contributions to the software must be
+#        published under this license. The University of Paderborn
+#        is granted the non-exclusive right to publish modifications
+#        or contributions in future versions of the Software free of charge.
+#
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#     EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#     OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#     NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#     HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#     WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#     OTHER DEALINGS IN THE SOFTWARE.
+#
+#     Persons using the Software are encouraged to notify the
+#     Signal and System Theory Group at the University of Paderborn
+#     about bugs. Please reference the Software in your publications
+#     if it was used for them.
+
+
 import numpy as np
 import scipy as sc
 import scipy.linalg
@@ -41,6 +81,8 @@ def _jbss_sos(x, tau=0, cost_selection='cross'):
     Multiset Multidimensional Variables by Joint Diagonalization," Lecture Notes in Computer
     Science: Independent Component Analysis and Blind Signal Separation, Latent Variable Analysis
     and Signal Separation, Springer Berlin / Heidelberg, 2010, 6365, 197-204
+
+    Code is converted to Python by Isabell Lehmann (isabell.lehmann at sst.upb.de)
 
     """
 
@@ -121,6 +163,8 @@ def _diag_cross(T, W0=None, tol=1e-10):
     -------
     W : np.ndarray
         demixing matrix of dimensions N x N x K
+
+    Code is converted to Python by Isabell Lehmann (isabell.lehmann at sst.upb.de)
 
     """
 
@@ -211,6 +255,8 @@ def _diag_whole(T, W0, tol=1e-5):
     W : np.ndarray
         demixing matrix of dimensions N x N x K
 
+    Code is converted to Python by Isabell Lehmann (isabell.lehmann at sst.upb.de)
+
     """
 
     W = np.copy(W0)
@@ -242,6 +288,8 @@ def _iva_diag_kernel(T, W0, mu, tol=1e-5):
     -------
     W : np.ndarray
         demixing matrix of dimensions N x N x K
+
+    Code is converted to Python by Isabell Lehmann (isabell.lehmann at sst.upb.de)
 
     """
 
@@ -338,6 +386,8 @@ def _sym_diag_cross(T, W0=None, tol=1e-10):
     W : np.ndarray
         demixing matrix of dimensions N x N x K
 
+    Code is converted to Python by Isabell Lehmann (isabell.lehmann at sst.upb.de)
+
     """
 
     N, _, K, _, L = T.shape
@@ -416,6 +466,8 @@ def _cca(X):
     -------
     W : np.ndarray
         transformation matrices for the two datasets, with dimensions N x N x 2
+
+    Code is converted to Python by Isabell Lehmann (isabell.lehmann at sst.upb.de)
 
     """
 
