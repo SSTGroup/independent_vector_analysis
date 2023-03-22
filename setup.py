464aeebe069c6c22eedb0113f785ad7773ed96ec
Isabell Lehmann <isabell.lehmann@sst.upb.de>
2023-03-22 12:16:29 +0100

    changes in covariance generation

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='independent_vector_analysis',
-      version='0.3.0',
+      version='0.3.1',
       description='Implementation of IVA-G and IVA-L-SOS',
       long_description=readme(),
       long_description_content_type='text/markdown',
